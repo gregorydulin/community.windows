2646c3ae3926795c6cdf8b596757b627098c6bcc
Gregory M. Dulin <g.dulin@striveworks.us>
2021-07-21 08:44:37 -0500

    Fix win_domain_user in edge-case environments
    Fixes https://github.com/ansible-collections/community.windows/issues/116
    Fixes https://github.com/ansible-collections/community.windows/issues/142

--- plugins/modules/win_domain_user.ps1
+++ plugins/modules/win_domain_user.ps1
@@ -127,7 +127,7 @@ if ($null -ne $domain_server) {
 Function Get-PrincipalGroups {
     Param ($identity, $args_extra)
     try{
-        $groups = Get-ADPrincipalGroupMembership -Identity $identity @args_extra -ErrorAction Stop
+        $groups = Get-ADUser -Identity $name @extra_args -Properties memberOf | Select-Object -ExpandProperty memberOf | Get-ADGroup @extra_args
     } catch {
         Add-Warning -obj $result -message "Failed to enumerate user groups but continuing on.: $($_.Exception.Message)"
         return @()
